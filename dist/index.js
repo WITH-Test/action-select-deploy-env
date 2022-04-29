03e932a702d271ad1975f9cd58a271c57c63c9cc
Carlos Gonzalez <carlos.gonzalez@with-madrid.com>
2022-04-29 18:35:24 +0200

    select env in feature branches

--- dist/index.js
+++ dist/index.js
@@ -4701,21 +4701,64 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_1__);
 
 
+const FEATURE_RE = /^feature\/[a-z]+(?<number>\d+)_/g;
+const VERSION_RE = /^v?\d+\.\d+\.\d+$/;
+const UNSAFE_CHAR = /[^\da-z]+/g;
 process.on("unhandledRejection", handleError);
 main().catch(handleError);
-const FEATURE_RE = new RegExp('^feature/[a-z]+(\d+)_');
-const VERSION_RE = new RegExp('^v?\d+\.\d+\.\d+$');
-const UNSAFE_CHAR = new RegExp('[^\da-z]+');
+function refInfo(ref) {
+    const isTag = ref.startsWith('refs/tags/');
+    if (isTag) {
+        const namedTag = ref.replace(/^refs\/tags\//, '');
+        if (VERSION_RE.test(namedTag)) {
+            return {
+                environment: "production",
+                key: namedTag
+            };
+        }
+    }
+    const isBranch = ref.startsWith('refs/heads/');
+    if (isBranch) {
+        const branchName = ref.replace(/^refs\/heads\//, '');
+        if (['master', 'main'].includes(branchName)) {
+            return {
+                environment: "staging",
+                key: 'main'
+            };
+        }
+        if ('develop' === branchName) {
+            return {
+                environment: "develop",
+                key: branchName
+            };
+        }
+        if (FEATURE_RE.test(branchName)) {
+            // TS regexp (named) capturing groups suck. This is ugly but it works
+            let branchNumber = "";
+            const handleMatch = function (...params /* $0, $1, $2 offset, input, groups */) {
+                // groups is the last argument in the variadic signature.
+                const groups = params.pop();
+                branchNumber = groups.number;
+                return groups.number;
+            };
+            branchName.replace(FEATURE_RE, handleMatch);
+            if (branchNumber) {
+                return {
+                    environment: "feature",
+                    key: `dev_${branchNumber}`
+                };
+            }
+        }
+    }
+    const friendlyRef = ref.split('/').slice(2).join('/');
+    throw new Error("Something went terribly wrong, trying to select " +
+        `an environment from ref ${friendlyRef}, but this pattern is not recognized.` +
+        " I suggest you bring cookies to the person in charge of builds.");
+}
 async function main() {
-    console.log('Context', _actions_github__WEBPACK_IMPORTED_MODULE_1__.context);
-    const trigger = _actions_github__WEBPACK_IMPORTED_MODULE_1__.context.ref;
-    let env;
-    if (undefined === env) {
-        _actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed("Something went terribly wrong, trying to generate sub-domain for branch {}, but this pattern of branch doesn't exist. I suggest you bring cookies to the person in charge of builds.");
-    }
-    else {
-        _actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput("environment", env);
-    }
+    const project = refInfo(_actions_github__WEBPACK_IMPORTED_MODULE_1__.context.ref);
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput("environment", project.environment);
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.setOutput("key", project.key);
 }
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function handleError(err) {
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,81 @@
 import * as core from "@actions/core";
 import {context} from "@actions/github";
 
+const FEATURE_RE: RegExp = /^feature\/[a-z]+(?<number>\d+)_/g
+const VERSION_RE: RegExp = /^v?\d+\.\d+\.\d+$/
+const UNSAFE_CHAR: RegExp = /[^\da-z]+/g
+
 process.on("unhandledRejection", handleError);
 main().catch(handleError);
 
-const FEATURE_RE: RegExp = new RegExp('^feature/[a-z]+(\d+)_')
-const VERSION_RE: RegExp = new RegExp ('^v?\d+\.\d+\.\d+$')
-const UNSAFE_CHAR: RegExp = new RegExp('[^\da-z]+')
+interface Project {
+  environment: string
+  key: string
+}
 
+function refInfo(ref: string): Project {
+  const isTag = ref.startsWith('refs/tags/')
+  if (isTag) {
+    const namedTag = ref.replace(/^refs\/tags\//, '')
+    if (VERSION_RE.test(namedTag)) {
+      return {
+        environment: "production",
+        key: namedTag
+      }
+    }
+  }
+
+  const isBranch = ref.startsWith('refs/heads/')
+  if (isBranch) {
+    const branchName = ref.replace(/^refs\/heads\//, '')
+
+    if (['master', 'main'].includes(branchName)) {
+      return {
+        environment: "staging",
+        key: 'main'
+      }
+    }
+
+    if ('develop' === branchName) {
+      return {
+        environment: "develop",
+        key: branchName
+      }
+    }
+
+    if (FEATURE_RE.test(branchName)) {
+
+      // TS regexp (named) capturing groups suck. This is ugly but it works
+      let branchNumber: string = ""
+      const handleMatch = function (...params: any[] /* $0, $1, $2 offset, input, groups */) {
+        // groups is the last argument in the variadic signature.
+        const groups = params.pop();
+        branchNumber = groups.number
+        return groups.number;
+      }
+
+      branchName.replace(FEATURE_RE, handleMatch)
+
+      if (branchNumber) {
+        return {
+          environment: "feature",
+          key: `dev_${branchNumber}`
+        }
+      }
+    }
+  }
+
+  const friendlyRef = ref.split('/').slice(2).join('/')
+  throw new Error("Something went terribly wrong, trying to select " +
+    `an environment from ref ${friendlyRef}, but this pattern is not recognized.` +
+    " I suggest you bring cookies to the person in charge of builds.")
+}
 
 
 async function main(): Promise<void> {
-  console.log('Context', context)
-
-  const trigger = context.ref
-
-  let env
-
-  if (undefined === env) {
-    core.setFailed("Something went terribly wrong, trying to generate sub-domain for branch {}, but this pattern of branch doesn't exist. I suggest you bring cookies to the person in charge of builds.")
-  } else {
-
-    core.setOutput("environment", env);
-  }
+  const project = refInfo(context.ref)
+  core.setOutput("environment", project.environment);
+  core.setOutput("key", project.key)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
